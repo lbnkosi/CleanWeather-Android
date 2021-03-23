47723073ebf8b5966590f4a1af289e48cf124971
Lebogang Nkosi <lebogang@lbnkosi.co.za>
2021-03-23 09:00:59 +0200

    Removed App Id from network constants
    Removed App Id from network constants

--- data/src/main/java/com/lbnkosi/data/utils/NetworkConstants.kt
+++ data/src/main/java/com/lbnkosi/data/utils/NetworkConstants.kt
@@ -2,6 +2,7 @@ package com.lbnkosi.data.utils
 
 object NetworkConstants {
 
-    const val APP_ID = "44377a3100450321bdf12cdfbed45521"
+    //TODO replace this with your own
+    const val APP_ID = ""
 
 }
